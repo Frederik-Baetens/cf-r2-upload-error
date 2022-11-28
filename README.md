f2d4a1c93c0310df3471210d7ee56d921a771ab3
Antoine COUETTE <a.j.couette@gmail.com>
2022-11-28 21:44:40 +0100

    script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # cf-r2-upload-error
+
+The script needs the following env vars to be set
+
+```
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+CF_BUCKET_NAME
+CF_OBJECT_KEY
+```
+
+
+
---
+++ reproduce_failure.js
@@ -0,0 +1,112 @@
+const {S3} = require('aws-sdk');
+const {stat, open} = require('fs').promises;
+const axios = require('axios');
+const crypto = require('crypto');
+
+const options = {
+    // accessKeyId: 'xxx',
+    // secretAccessKey: 'xxx',
+    endpoint: `https://367791ca7abea81096902b345fee7b1f.r2.cloudflarestorage.com`,
+    signatureVersion: 'v4',
+    region: 'auto',
+};
+
+
+const key = process.env.CF_OBJECT_KEY;
+const bucket = process.env.CF_BUCKET_NAME;
+const partSize = 5242880;
+const objectSize = 24262281;
+const failureRatePercentageOnUploadEvent = 30;
+
+async function doIt() {
+
+    try {
+        const s3 = new S3(options);
+
+
+        const createMultipartUploadResponse = await s3.createMultipartUpload({
+            Bucket: bucket,
+            Key: key
+        }).promise();
+
+
+        const listParts = [];
+        let index = 0;
+        do {
+
+            try{
+                let start = index * partSize;
+                console.log('start upload part ' + (index + 1));
+
+                const uploadPartResponse = await s3.getSignedUrlPromise('uploadPart', {
+                    Bucket: bucket,
+                    Key: key,
+                    UploadId: createMultipartUploadResponse.UploadId,
+                    PartNumber: index + 1
+                });
+
+                let end = Math.min(objectSize, (index + 1) * partSize);
+
+                const controller = axios.CancelToken.source();
+
+                const response = await axios.put(uploadPartResponse, crypto.randomBytes(end - start), {
+                    cancelToken: controller.token,
+                    onUploadProgress: function (e) {
+                        if(Math.random() * 100 > (100 - failureRatePercentageOnUploadEvent)){
+                            controller.cancel();
+                        }
+                    },
+                    headers: {
+                        'Content-length': end - start,
+                    }
+                });
+
+
+                let header = response.headers['etag'];
+
+                console.log('part '+(index+1) + 'part success, etag: '+header)
+
+                listParts.push({
+                    ETag: header,
+                    PartNumber: index + 1,
+                })
+
+                index += 1;
+            }catch (e){
+                console.log('catch error, without incrementing index so part is going to be restarted');
+            }
+
+        } while (index * partSize < objectSize)
+
+
+        let params = {
+            Bucket: bucket,
+            Key: key,
+            UploadId: createMultipartUploadResponse.UploadId,
+            MultipartUpload: {
+                Parts: listParts
+            }
+        };
+
+        console.log('completing with the following params : ', JSON.stringify(params))
+
+        await s3.completeMultipartUpload(params).promise();
+
+    } catch (e) {
+        if (e.response) {
+            console.log({
+                errorData: e.response?.data,
+                errorStatus: e.response?.status,
+                errorHeaders: e.response?.headers,
+            })
+        } else {
+            console.error(e)
+        }
+    }
+
+}
+
+doIt();
+
+
+// "AWS_ACCESS_KEY_ID=$SCW_ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SCW_SECRET_KEY AWS_REGION=${var.region} BUCKET=${scaleway_object_bucket.bucket[count.index].name} node lifecycle/put-pucket-lifcycle.js"
